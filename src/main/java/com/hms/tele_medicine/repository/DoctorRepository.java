22a50cbe6d3fcdca6edb9fad3ffa98efbd7bc28b
Aman Agarwal <amanag0101@gmail.com>
2023-05-20 11:24:50 +0530

    Fix search query for login of users

--- src/main/java/com/hms/tele_medicine/repository/DoctorRepository.java
+++ src/main/java/com/hms/tele_medicine/repository/DoctorRepository.java
@@ -2,11 +2,14 @@ package com.hms.tele_medicine.repository;
 
 import com.hms.tele_medicine.entity.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+    @Query(value = "SELECT * FROM doctors " +
+            "WHERE BINARY email = :email AND BINARY password = :password", nativeQuery = true)
     Optional<Doctor> findByEmailAndPassword(String email, String password);
 }
--- src/main/java/com/hms/tele_medicine/repository/PatientRepository.java
+++ src/main/java/com/hms/tele_medicine/repository/PatientRepository.java
@@ -2,9 +2,12 @@ package com.hms.tele_medicine.repository;
 
 import com.hms.tele_medicine.entity.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+    @Query(value = "SELECT * FROM patients " +
+            "WHERE BINARY email = :email AND BINARY password = :password", nativeQuery = true)
     Optional<Patient> findByEmailAndPassword(String email, String password);
 }
